8888ebf5093cf49083d7ccc19b2f77c4d521ac06
Tobias <tbp@kontrapunkt.com>
2020-02-12 16:40:39 +0100

    Added custom exceptions to __init__.py to allow for use from outside the package itself

--- float_api/__init__.py
+++ float_api/__init__.py
@@ -1,4 +1,6 @@
 from .float_api import FloatAPI
+from .float_api import UnexpectedStatusCode
+from .float_api import DataValidationError
 
 
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="float_api",
-    version="0.0.5",
+    version="0.0.7",
     author="Tobias Balle-Petersen",
     author_email="tobiasbp@gmail.com",
     license="MIT",
